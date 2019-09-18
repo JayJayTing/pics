969284748e7d346d29e95c51fce0e6af6df15750
JayJayTing <tingjayj@gmail.com>
2019-09-17 22:12:16 -0400

    moving

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 function App() {
-	function onSearchSubmit(term) {
-		axios.get("https://api.unsplash.com/search/photos", {
+	async function onSearchSubmit(term) {
+		const response = await axios.get("https://api.unsplash.com/search/photos", {
 			params: { query: term },
 			headers: {
 				Authorization:
 					"Client-ID d013c75a1cd43e3a01e266b8a0a847f6104ab9fb594a5e2595dab97fbcdfd56d"
 			}
 		});
+
+		console.log(response.data.results);
 	}
 	return (
 		<div className="ui container" style={{ marginTop: "10px" }}>
